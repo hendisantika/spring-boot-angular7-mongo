90aff5a2ccc2c572883c3ac09478f977d9e69655
Hendi Santika <hendisantika@gmail.com>
2018-11-12 03:57:10 +0700

    #5 - Add Contact Repository - contact-app-backend

---
+++ contact-app-backend/src/main/java/com/hendisantika/contactappbackend/repository/ContactRepository.java
@@ -0,0 +1,20 @@
+package com.hendisantika.contactappbackend.repository;
+
+import com.hendisantika.contactappbackend.model.Contact;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : contact-app-backend
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/11/18
+ * Time: 03.56
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ContactRepository extends CrudRepository<Contact, String> {
+    Contact findOne(String id);
+
+    void delete(Contact deleted);
+}
