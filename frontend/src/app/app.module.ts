8dc310bfc7f43f810c0c9e214c036dcfb892b307
Hendi Santika <hendisantika@gmail.com>
2018-11-17 09:45:07 +0700

    #12 - Set Router module - Angular Frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,21 @@ import {AppComponent} from './app.component';
 import {ContactComponent} from './contact/contact.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
+
+
+const appRoutes: Routes = [
+  {
+    path: 'contact',
+    component: ContactComponent,
+    data: {title: 'Contact List'}
+  },
+  {
+    path: '',
+    redirectTo: '/contact',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
@@ -13,7 +28,11 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
