a4c58ba7fc79e09bacc787760d44b5d53c0038a2
Hendi Santika <hendisantika@gmail.com>
2018-11-18 06:13:25 +0700

    #22 - Modify index.js

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './App.css';
+import Edit from './components/Edit';
+import Create from './components/Create';
+import Show from './components/Show';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route exact path='/' component={App}/>
+            <Route path='/edit/:id' component={Edit}/>
+            <Route path='/create' component={Create}/>
+            <Route path='/show/:id' component={Show}/>
+        </div>
+    </Router>,
+    document.getElementById('root')
+);
