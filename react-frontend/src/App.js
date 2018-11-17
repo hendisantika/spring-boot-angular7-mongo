e4c6cfffd96df7b57a6ddbbf9eed0674164c28ea
Hendi Santika <hendisantika@gmail.com>
2018-11-18 06:14:36 +0700

    #23 - Modify app.js

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,25 +1,54 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            contacts: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('/contacts')
+            .then(res => {
+                this.setState({contacts: res.data});
+                console.log(this.state.contacts);
+            });
+    }
+
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+            <div class="container">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">
+                            CONTACTS LIST
+                        </h3>
+                    </div>
+                    <div class="panel-body">
+                        <h4><Link to="/create"><span class="glyphicon glyphicon-plus-sign"
+                                                     aria-hidden="true"></span> Add Contact</Link></h4>
+                        <table class="table table-stripe">
+                            <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Address</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {this.state.contacts.map(c =>
+                                <tr>
+                                    <td><Link to={`/show/${c.id}`}>{c.name}</Link></td>
+                                    <td>{c.address}</td>
+                                </tr>
+                            )}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         );
     }
